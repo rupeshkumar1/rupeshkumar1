d3c5d8d840f69d76dd1d8438a8672809973b6a5e
rupeshkumar1 <97241843+rupeshkumar1@users.noreply.github.com>
2022-01-07 21:18:21 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php session_start();
+$_SESSION['page']="home";
+?>
+<html>
+    <head>
+        <title>Help me in getting Funds!</title>
+        <link rel="stylesheet" href="style.css">
+        <meta name="viewport" content="width-device-width, initial-scale=1">
+    </head>
+    <body style="background-color: black">
+        <header>
+            <?php include './Header.php'; ?>
+<br>
+<div style="font-family: 'Montserrat',sans-serif;" class="container">
+                    <h1>THE SPARKS FOUNDATION PROJECT </h1><br>
+                    <p><h3>"Payment Gateway Integration"<br>The Sparks Foundation aims to inspire students, help them innovate and let them integrate to build the next generation humankind. <br></h3></p>
+                    <br><h3 style="text-indent:15em;";> By: RUPESH KUMAR</h3>
+                </div>
+              </div>
+        </header>
+    </body>
+</html>
